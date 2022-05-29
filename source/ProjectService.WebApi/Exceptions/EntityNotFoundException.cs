7880f3c85f50d499220cedf43fa39e744baf1120
Alexander Blazhkov <thermo911@yandex.ru>
2022-05-29 19:31:32 +0300

    feat: generic exceptions

---
+++ source/ProjectService.WebApi/Exceptions/EntityNotFoundException.cs
@@ -0,0 +1,24 @@
+using System.Text;
+
+namespace ProjectService.WebApi.Exceptions;
+
+public class EntityNotFoundException<T> : Exception
+    where T : class
+{
+    public EntityNotFoundException(object primaryKey, params object[] primaryKeys) 
+        : base(BuildMessage(primaryKey, primaryKeys))
+    {
+    }
+    
+    private static string BuildMessage(object primaryKey, params object[] primaryKeys)
+    {
+        var sb = new StringBuilder($"Entity of type {typeof(T)} with primary key(s) {{{primaryKey}");
+        foreach (object key in primaryKeys)
+        {
+            sb.Append($", {key}");
+        }
+
+        sb.Append("} not found.");
+        return sb.ToString();
+    }
+}
