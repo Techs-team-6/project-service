34479e11b2773becf24df7a70b8e4eaeba8eab08
Bill Williams <eid20021@gmail.com>
2022-06-02 22:30:03 +0300

    feat: download file by storage id

--- source/ProjectService.WebApi/Interfaces/IProjectService.cs
+++ source/ProjectService.WebApi/Interfaces/IProjectService.cs
@@ -8,6 +8,7 @@ public interface IProjectService
     public Task<Uri> AddProjectAsync(ProjectCreateDto project);
     public Task<ProjectBuild> CreateVersionAsync(Guid projectId);
     public Stream GetProjectVersionArchive(Guid projectId, int buildId);
+    public Stream GetProjectVersionArchive(Guid storageId);
     public string? UpdateBuildString(Guid projectId, string newBuildString);
     public GitInfo GetGitInfo();
 }
