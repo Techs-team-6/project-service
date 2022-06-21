dd5d30d9a41462b16baae6bd4482c42bc01e669d
Alexander Blazhkov <thermo911@yandex.ru>
2022-06-21 09:49:36 +0300

    refactor: remove public modifier from interfaces

--- source/ProjectService.Core/Interfaces/IProjectService.cs
+++ source/ProjectService.Core/Interfaces/IProjectService.cs
@@ -5,12 +5,12 @@ namespace ProjectService.Core.Interfaces;
 
 public interface IProjectService
 {
-    public Task<Uri> AddProjectAsync(ProjectCreateDto project);
-    public Task<ProjectBuild> CreateVersionAsync(Guid projectId);
+    Task<Uri> AddProjectAsync(ProjectCreateDto project);
+    Task<ProjectBuild> CreateVersionAsync(Guid projectId);
     Task<Project> GetProjectAsync(Guid projectId);
-    public Stream GetProjectVersionArchive(Guid projectId, int buildId);
-    public Stream GetProjectVersionArchive(Guid storageId);
-    public string? UpdateBuildString(Guid projectId, string newBuildString);
-    public GitInfo GetGitInfo();
+    Stream GetProjectVersionArchive(Guid projectId, int buildId);
+    Stream GetProjectVersionArchive(Guid storageId);
+    string? UpdateBuildString(Guid projectId, string newBuildString);
+    GitInfo GetGitInfo();
     Task DeleteProjectAsync(Guid projectId);
 }
