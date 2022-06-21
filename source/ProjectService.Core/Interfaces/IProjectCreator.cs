82ff51cdf77393090d065cb41ae9361fd8591376
Alexander Blazhkov <68013846+thermo911@users.noreply.github.com>
2022-06-21 09:51:28 +0300

    refactor: remove public modifier from interfaces (#23)

--- source/ProjectService.Core/Interfaces/IProjectCreator.cs
+++ source/ProjectService.Core/Interfaces/IProjectCreator.cs
@@ -2,5 +2,5 @@ namespace ProjectService.Core.Interfaces;
 
 public interface IProjectCreator
 {
-    public Task<string> CreateAsync(string path, string projectName);
+    Task<string> CreateAsync(string path, string projectName);
 }
